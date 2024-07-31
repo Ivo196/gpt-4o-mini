0bad6bf01710837b74b5a9242e8d8276456ccd2a
Ivo196 <ivo.tonioni@gmail.com>
2024-07-31 22:29:09 +0200

    add structured Parse

--- output-parsers.js
+++ output-parsers.js
@@ -2,6 +2,8 @@ import { ChatOpenAI } from "@langchain/openai";
 import { config } from "dotenv";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
+import { StructuredChatOutputParser } from "langchain/agents";
+import { StructuredOutputParser } from "langchain/output_parsers";
 
 config()
 
@@ -19,14 +21,37 @@ const prompt =  ChatPromptTemplate.fromMessages([
     ['human','{input}']
 ])
 
-// Create the parser
-const parser = new StringOutputParser()
+// Structured otuput parser 
+async function callStructuredParser() {
+    const prompt = ChatPromptTemplate.fromTemplate(`
+        Extract information from the following phrase. 
+        Formatting Instructions: {format_instructions} 
+        Phrase:{phrase}
+    `)
+    const outputParser = StructuredOutputParser.fromNamesAndDescriptions({ //Esto es lo que convierte en format_instructions y se lo paso a la prompt
+        name: 'The name of the person',
+        age: 'The age of the person'
+    })
 
-// Create a chain 
-const chain =  prompt.pipe(model).pipe(parser)
+    const chain = prompt.pipe(model).pipe(outputParser)
 
-// Call the chain
-const responde = await chain.invoke({
-    input:'Dog'})
+    return await chain.invoke({
+        format_instructions:outputParser.getFormatInstructions(),
+        phrase:'Max is 30 years old'
+    })
+}
 
-console.log(responde)
+// // Create the parser
+// const parser = new StringOutputParser()
+
+// // Create a chain 
+// const chain =  prompt.pipe(model).pipe(parser)
+
+// // Call the chain
+// const responde = await chain.invoke({
+//     input:'Dog'})
+
+// console.log(responde)
+
+const response = await callStructuredParser() //Observamos que devuelve un objeto de js
+console.log(response)

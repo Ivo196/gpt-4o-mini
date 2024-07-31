cdb138561c6fcd415719b523e11797de5b9cb725
Ivo196 <ivo.tonioni@gmail.com>
2024-07-31 19:39:59 +0200

    add output-parsers

--- output-parsers.js
+++ output-parsers.js
@@ -1,29 +1,29 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { config } from "dotenv";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
 config()
 
 const api = process.env.API_KEY
-
+//Instanciamos el modelo 
 const model = new ChatOpenAI({
     openAIApiKey:api,
     modelName:'gpt-4o-mini',
     temperature:0.7,
 })
 
-// Create a prompt template 1
-const prompt =  ChatPromptTemplate.fromTemplate('You are a comidian. Tell a joke following the next word {input}')
-// console.log(await prompt.format({word:'chicken'})) //Vemos el formato del prompt
-
-// Create a prompt template 2
-const prompt2 =  ChatPromptTemplate.fromMessages([
+// Create a prompt template
+const prompt =  ChatPromptTemplate.fromMessages([
     ['system','Generate a joke based on the word provided by the user.'],
     ['human','{input}']
 ])
 
+// Create the parser
+const parser = new StringOutputParser()
+
 // Create a chain 
-const chain =  prompt2.pipe(model)
+const chain =  prompt.pipe(model).pipe(parser)
 
 // Call the chain
 const responde = await chain.invoke({
